ac17ec82ea17e62721b02982097eebe37e2fa5eb
Florian Dewitz <fdewitz@allinsight.de>
2023-10-13 19:11:04 +0200

    Chnages have been made

--- KubeRequest/Program.cs
+++ KubeRequest/Program.cs
@@ -11,8 +11,15 @@ namespace KubeRequest
         {
             var config = KubernetesClientConfiguration.InClusterConfig();
             var client = new Kubernetes(config);
-
-            var pods = client.CoreV1.ListNamespacedPod("default");
+            KubeService kbs = new KubeService();
+            kbs.listPodIps().ForEach(x => Console.WriteLine(x));
+            var th = new Thread(kbs.receiveMessage);
+            th.Start();
+            while (true)
+            {
+                kbs.sendMessage();
+                Thread.Sleep(10000);
+            }
         }
     }
 
@@ -29,7 +36,7 @@ namespace KubeRequest
             httpClient = new HttpClient();
         }
 
-        private List<string> listPodIps()
+        public List<string> listPodIps()
         {
             List<string> ips = new List<string>();
             foreach (var pod in this.client.CoreV1.ListNamespacedPod("default").Items)
@@ -41,27 +48,36 @@ namespace KubeRequest
 
         public void sendMessage()
         {
-            var httpClient = new HttpClient();
             List<string> list = listPodIps();
             foreach (var ips in list)
             {
-                httpClient.BaseAddress = new Uri(ips);
+                if (ips.Equals(Environment.GetEnvironmentVariable("podIP"))) continue;
+                httpClient.BaseAddress = new Uri($"http://{ips}:80/");
                 HttpRequestMessage msg = new HttpRequestMessage(HttpMethod.Post, httpClient.BaseAddress);
-                msg.Content = new StringContent("Pong!");
-                httpClient.Send(msg);
+                msg.Content = new StringContent("Ping!");
+                Console.WriteLine("Request sending...");
+                HttpResponseMessage response = httpClient.Send(msg);
+                string responseMsg = new StreamReader(response.Content.ReadAsStream()).ReadToEnd();
+                Console.WriteLine($"Request response: {responseMsg}");
             }
                 
         }
 
         public void receiveMessage()
         {
-            httpClient.BaseAddress = new Uri("test");
             HttpListener listenr = new HttpListener();
             listenr.Start();
-            var req = listenr.GetContext().Request;
-            string text;
-            StreamReader reader = new StreamReader();
-            req.InputStream.BeginRead();
+            listenr.Prefixes.Add("http://*:80/");
+            var reqContext = listenr.GetContext();
+            string reqText = new StreamReader(reqContext.Request.InputStream).ReadToEnd();
+            Console.WriteLine($"Request Comming in: {reqText}");
+            byte[] responseBytes = Encoding.UTF8.GetBytes("Pong!");
+            reqContext.Response.StatusCode = 200;
+            reqContext.Response.KeepAlive = false;
+            reqContext.Response.ContentLength64 = responseBytes.Length;
+            reqContext.Response.OutputStream.Write(responseBytes, 0, responseBytes.Length);
+            reqContext.Response.Close();
+            Console.WriteLine("Response sending...");
         }
     }
 }
